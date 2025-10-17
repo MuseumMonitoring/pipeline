2fb903d143a08eba1ccdefb8e87a6cf41da53e3d
ajuvercr <arthur.vercruysse@outlook.com>
2025-10-17 11:40:56 +0200

    feat: update mapper

--- src/builder.ts
+++ src/builder.ts
@@ -1,4 +1,4 @@
-import { Quad, Quad_Object, Quad_Predicate, Quad_Subject, Term } from "@rdfjs/types";
+import { Quad, Quad_Graph, Quad_Object, Quad_Predicate, Quad_Subject, Term } from "@rdfjs/types";
 
 import { DataFactory } from "n3";
 const { quad, blankNode } = DataFactory;
@@ -6,24 +6,26 @@ const { quad, blankNode } = DataFactory;
 export class Builder {
     readonly id: Term;
     readonly quads: Quad[];
-    constructor(id: Term, quads: Quad[]) {
+    readonly graph: Term | undefined;
+    constructor(id: Term, quads: Quad[], graph?: Term) {
         this.id = id;
         this.quads = quads;
+        this.graph = graph;
     }
 
     triple(pred: Term): Builder;
     triple(pred: Term, object: Term): Builder;
     triple(pred: Term, object?: Term): Builder {
         const o = object === undefined ? blankNode() : object;
-        this.quads.push(quad(<Quad_Subject>this.id, <Quad_Predicate>pred, <Quad_Object>o));
-        return new Builder(o, this.quads);
+        this.quads.push(quad(<Quad_Subject>this.id, <Quad_Predicate>pred, <Quad_Object>o, <Quad_Graph>this.graph));
+        return new Builder(o, this.quads, this.graph);
     }
 
     tripleThis(pred: Term): Builder;
     tripleThis(pred: Term, object: Term): Builder;
     tripleThis(pred: Term, object?: Term): Builder {
         const o = object === undefined ? blankNode() : object;
-        this.quads.push(quad(<Quad_Subject>this.id, <Quad_Predicate>pred, <Quad_Object>o));
+        this.quads.push(quad(<Quad_Subject>this.id, <Quad_Predicate>pred, <Quad_Object>o, <Quad_Graph>this.graph));
         return this;
     }
 }
